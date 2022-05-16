58912d0290f09201fda1944ee1c803e64dd6dfb9
faturita <rramele@gmail.com>
2022-05-16 16:20:58 +0100

    Adding more comments.

--- src/addressbook.cpp
+++ src/addressbook.cpp
@@ -8,11 +8,14 @@
 //  Time to completion: 4 hours and 20 minutes.
 //
 //
-//  The main is just a wrapper for the functionalities provided by the AddressBook class, based on command line parameters.  It can add new
+//  The main provides the access to the functionalities provided by the AddressBook class, based on command line parameters.  It can add new
 //  records, list them, remove one by one, find a particular one based on partial matching for name or lastname.
 //
+//  The AddressBook class is based on vector<> template class from std14+.  At the end is just that and relies heavily on what the vector template
+//  can do or not.  It is quite straightforward.
+//
 //  The idea is to load everything into memory, operate on a standard vector structure, and stores everything back to a file.
-//  This is inefficient to store a lot of information, but it is very quick, it works like a in-memory database.
+//  This way is not very efficient for a large number of records.  However, as it is like an in-memory database, it is very quick.
 //  For large files, we start to move into the realm of databases so we need to use new approaches to deal with large registers.  We can start
 //  by fixing each record to a fixed-length approach, create an index of the file, and move around based on the index.
 
